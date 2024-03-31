76e92ad5421eb373958e04a1adfabb0b33211fcc
tauhiddewan <dewan.tauhid.rahman@gmail.com>
2024-03-31 10:37:33 +0600

    added pg database uri

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-db_url = "sqlite:///database.db"
+# db_url = "sqlite:///database.db"
+db_url = "postgresql://postgres:pgadmin@127.0.0.1:5432/my_pgdb"
 
 engine = create_engine(db_url)
 
